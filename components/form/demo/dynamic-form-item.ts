813445b57bb3a3bca3bbadbe39918a171d1768f4
Gradii Robot <gradii.robot@users.noreply.github.com>
2025-08-28 12:47:27 +0000

    build: merge changes from origin_master

--- components/form/demo/dynamic-form-item.ts
+++ components/form/demo/dynamic-form-item.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormControl, FormRecord, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { TriButtonModule } from 'ng-zorro-antd/button';
 import { TriFormModule } from 'ng-zorro-antd/form';
@@ -11,42 +11,54 @@ import { TriInputModule } from 'ng-zorro-antd/input';
   imports: [ReactiveFormsModule, TriButtonModule, TriFormModule, TriIconModule, TriInputModule],
   template: `
     <form tri-form [formGroup]="validateForm" (ngSubmit)="submitForm()">
-      @for (control of listOfControl; track control; let i = $index) {
+      <ng-container formArrayName="names">
+        @for (control of listOfControl.controls; track control; let i = $index) {
+          <tri-form-item>
+            @if (i === 0) {
+              <tri-form-label [xs]="24" [sm]="4" [for]="'passenger' + i"> Passengers </tri-form-label>
+            }
+            <tri-form-control
+              [xs]="24"
+              [sm]="20"
+              [offset]="i === 0 ? 0 : 4"
+              errorTip="Please input passenger's name or delete this field."
+            >
+              <input
+                class="passenger-input"
+                tri-input
+                placeholder="placeholder"
+                [attr.id]="'passenger' + i"
+                [formControlName]="i"
+              />
+              @if (listOfControl.controls.length > 1) {
+                <tri-icon type="minus-circle-o" class="dynamic-delete-button" (click)="removeField(i, $event)" />
+              }
+            </tri-form-control>
+          </tri-form-item>
+        }
+
         <tri-form-item>
-          @if (i === 0) {
-            <tri-form-label [xs]="24" [sm]="4" [for]="control.controlInstance"> Passengers </tri-form-label>
-          }
-          <tri-form-control
-            [xs]="24"
-            [sm]="20"
-            [offset]="i === 0 ? 0 : 4"
-            errorTip="Please input passenger's name or delete this field."
-          >
-            <input
-              class="passenger-input"
-              tri-input
-              placeholder="placeholder"
-              [attr.id]="control.id"
-              [formControlName]="control.controlInstance"
-            />
-            <tri-icon type="minus-circle-o" class="dynamic-delete-button" (click)="removeField(control, $event)" />
+          <tri-form-control [xs]="{ span: 24, offset: 0 }" [sm]="{ span: 20, offset: 4 }">
+            <button tri-button type="dashed" class="add-button" (click)="addField($event)">
+              <tri-icon type="plus" />
+              Add field
+            </button>
           </tri-form-control>
         </tri-form-item>
-      }
-
-      <tri-form-item>
-        <tri-form-control [xs]="{ span: 24, offset: 0 }" [sm]="{ span: 20, offset: 4 }">
-          <button tri-button type="dashed" class="add-button" (click)="addField($event)">
-            <tri-icon type="plus" />
-            Add field
-          </button>
-        </tri-form-control>
-      </tri-form-item>
-      <tri-form-item>
-        <tri-form-control [xs]="{ span: 24, offset: 0 }" [sm]="{ span: 20, offset: 4 }">
-          <button tri-button type="primary">Submit</button>
-        </tri-form-control>
-      </tri-form-item>
+        <tri-form-item>
+          <tri-form-control [xs]="{ span: 24, offset: 0 }" [sm]="{ span: 20, offset: 4 }">
+            <button tri-button type="dashed" class="add-button" (click)="addHeadField($event)">
+              <tri-icon type="plus" />
+              Add field at head
+            </button>
+          </tri-form-control>
+        </tri-form-item>
+        <tri-form-item>
+          <tri-form-control [xs]="{ span: 24, offset: 0 }" [sm]="{ span: 20, offset: 4 }">
+            <button tri-button type="primary">Submit</button>
+          </tri-form-control>
+        </tri-form-item>
+      </ng-container>
     </form>
   `,
   styles: [
@@ -81,41 +93,31 @@ import { TriInputModule } from 'ng-zorro-antd/input';
 })
 export class TriDemoFormDynamicFormItemComponent implements OnInit {
   private fb = inject(NonNullableFormBuilder);
-  validateForm: FormRecord<FormControl<string>> = this.fb.record({});
-  listOfControl: Array<{ id: number; controlInstance: string }> = [];
+  validateForm = this.fb.group({
+    names: this.fb.array([])
+  });
+  listOfControl = this.validateForm.get('names') as FormArray;
 
   addField(e?: MouseEvent): void {
     e?.preventDefault();
-
-    const id = this.listOfControl.length > 0 ? this.listOfControl[this.listOfControl.length - 1].id + 1 : 0;
-
-    const control = {
-      id,
-      controlInstance: `passenger${id}`
-    };
-    const index = this.listOfControl.push(control);
-    console.log(this.listOfControl[this.listOfControl.length - 1]);
-    this.validateForm.addControl(
-      this.listOfControl[index - 1].controlInstance,
-      this.fb.control('', Validators.required)
-    );
+    this.listOfControl.push(this.fb.control('', Validators.required));
   }
 
-  removeField(i: { id: number; controlInstance: string }, e: MouseEvent): void {
+  addHeadField(e?: MouseEvent): void {
+    e?.preventDefault();
+    this.listOfControl.insert(0, this.fb.control('The head item', Validators.required));
+  }
+
+  removeField(index: number, e: MouseEvent): void {
     e.preventDefault();
-    if (this.listOfControl.length > 1) {
-      const index = this.listOfControl.indexOf(i);
-      this.listOfControl.splice(index, 1);
-      console.log(this.listOfControl);
-      this.validateForm.removeControl(i.controlInstance);
-    }
+    this.listOfControl.removeAt(index);
   }
 
   submitForm(): void {
     if (this.validateForm.valid) {
       console.log('submit', this.validateForm.value);
     } else {
-      Object.values(this.validateForm.controls).forEach(control => {
+      Object.values(this.listOfControl.controls).forEach(control => {
         if (control.invalid) {
           control.markAsDirty();
           control.updateValueAndValidity({ onlySelf: true });
